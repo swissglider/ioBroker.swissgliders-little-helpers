144468d2aa9b6955505aa0c5627c18e5a97350e5
swissglider <npm@mailschweiz.com>
2020-11-19 14:41:14 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+    'swissgliders-little-helpers adapter settings': {
+        en: 'Adapter settings for swissgliders-little-helpers',
+        de: 'Adaptereinstellungen für swissgliders-little-helpers',
+        ru: 'Настройки адаптера для swissgliders-little-helpers',
+        pt: 'Configurações do adaptador para swissgliders-little-helpers',
+        nl: 'Adapterinstellingen voor swissgliders-little-helpers',
+        fr: "Paramètres d'adaptateur pour swissgliders-little-helpers",
+        it: "Impostazioni dell'adattatore per swissgliders-little-helpers",
+        es: 'Ajustes del adaptador para swissgliders-little-helpers',
+        pl: 'Ustawienia adaptera dla swissgliders-little-helpers',
+        'zh-cn': 'swissgliders-little-helpers的适配器设置',
+    },
+    option1: {
+        en: 'option1',
+        de: 'Option 1',
+        ru: 'Опция 1',
+        pt: 'Opção 1',
+        nl: 'Optie 1',
+        fr: 'Option 1',
+        it: 'opzione 1',
+        es: 'Opción 1',
+        pl: 'opcja 1',
+        'zh-cn': '选项1',
+    },
+    option2: {
+        en: 'option2',
+        de: 'Option 2',
+        ru: 'option2',
+        pt: 'opção 2',
+        nl: 'Optie 2',
+        fr: 'Option 2',
+        it: 'opzione 2',
+        es: 'opcion 2',
+        pl: 'Opcja 2',
+        'zh-cn': '选项2',
+    },
+};
